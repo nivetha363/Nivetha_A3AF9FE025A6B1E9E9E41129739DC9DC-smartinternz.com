b3659ea8536285236c1ce62f668af038979c985a
astvu346m22316 <127462698+nivetha363@users.noreply.github.com>
2023-09-25 15:09:03 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,36 +1,22 @@
-class BankAccount:
-  
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
+# Define the base class player 
+class player:
+  def play(self):
+    print("The player is playing cricket.")
     
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      # self.__account_balance =  self.__account_balance+amount
-      print("Deposited ₹{}. New balance: ₹{}".format(amount, self.__account_balance))
-    else:
-     print("Invalied deposit amount.")
-     
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      # self.__account_balance = self.__account_balance - amount
-      print("Withdrew ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
-    else:
-      print("Invalied withdraw amount or insufficient balance.")
+# Define the derived class batsman
+class Batsman(player):
+  def play(self):
+    print("The batsam is batting.")
 
-  def display_balance(self):
-    print("Account balance for {} (Account #{}): ₹{}".format(self.__account_holder_name, self.__account_number, self.__account_balance))
+# Define the derived class bowler
+class Bowler(player):
+  def play(self):
+      print("The bowler is bowling.")
+    
+# Create object of Batsman and Bowler Classes
+batsman = Batsman()
+bowler = Bowler()
 
-
-# Create an instance of the BankAccount class
-account = BankAccount(account_number="12345678", account_holder_name="Hari prabu", initial_balance=5000.0)
-
-
-# Test deposit and withdraw functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.display_balance()
+# Call the play() method for each object 
+batsman.play()
+bowler.play()
